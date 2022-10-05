75cab3f89436a6d33994081bb3ad592f850e6368
ksh940925 <ksh9409255@naver.com>
2022-10-05 11:45:21 +0900

    feat: init setting

---
+++ src/main/java/com/team6/onandthefarmorderservice/OnAndTheFarmOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnAndTheFarmOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnAndTheFarmOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/onandthefarmorderservice/OnAndTheFarmOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnAndTheFarmOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
